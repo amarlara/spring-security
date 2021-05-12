a109f8753b281ffeb3a6776017b42f3be3216576
Ranjith Amarlapudi <suchith1996@gmail.com>
2021-05-12 14:57:05 +0530

    feat(): use database with spring security

--- src/main/java/com/lithi/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/lithi/springsecurity/demo/config/DemoAppConfig.java
@@ -1,16 +1,33 @@
 package com.lithi.springsecurity.demo.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.lithi.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+	@Autowired
+	private Environment env;
+
+	private Logger logger = Logger.getLogger(getClass().getName());
+
 	// bean for ViewResolver
 	@Bean
 	public ViewResolver viewResolver() {
@@ -18,5 +35,31 @@ public class DemoAppConfig {
 		viewResolver.setPrefix("/WEB-INF/view/");
 		viewResolver.setSuffix(".jsp");
 		return viewResolver;
+	}
+
+	@Bean
+	public DataSource securityDataSource() {
+
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+		try {
+			// Log messages
+			logger.info("jdbc.url=" + env.getProperty("jdbc.url"));
+			logger.info("jdbc.user=" + env.getProperty("jdbc.user"));
+			// set properties
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+			securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+			securityDataSource.setUser(env.getProperty("jdbc.user"));
+			securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+			// set connection pool properties
+			securityDataSource.setInitialPoolSize(Integer.parseInt(env.getProperty("connection.pool.initialPoolSize")));
+			securityDataSource.setMinPoolSize(Integer.parseInt(env.getProperty("connection.pool.minPoolSize")));
+			securityDataSource.setMaxPoolSize(Integer.parseInt(env.getProperty("connection.pool.maxPoolSize")));
+			securityDataSource.setMaxIdleTime(Integer.parseInt(env.getProperty("connection.pool.maxIdleTime")));
+
+		} catch (PropertyVetoException exec) {
+			throw new RuntimeException(exec);
+		}
+		return securityDataSource;
 	}
 }
--- src/main/java/com/lithi/springsecurity/demo/config/DemoSecutiryConfig.java
+++ src/main/java/com/lithi/springsecurity/demo/config/DemoSecutiryConfig.java
@@ -1,27 +1,35 @@
 package com.lithi.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecutiryConfig extends WebSecurityConfigurerAdapter {
 
-	@SuppressWarnings("deprecation")
+	@Autowired
+	private DataSource secutiryDataSource;
+
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-		UserBuilder users = User.withDefaultPasswordEncoder();
-
-		auth.inMemoryAuthentication().withUser(users.username("lithi").password("lithi123").roles("EMPLOYEE"))
-				.withUser(users.username("Mouni").password("lithi123").roles("EMPLOYEE", "MANAGER"))
-				.withUser(users.username("Ranjith").password("lithi123").roles("EMPLOYEE", "ADMIN"));
-
+		/*
+		 * UserBuilder users = User.withDefaultPasswordEncoder();
+		 * 
+		 * auth.inMemoryAuthentication().withUser(users.username("lithi").password(
+		 * "lithi123").roles("EMPLOYEE"))
+		 * .withUser(users.username("Mouni").password("lithi123").roles("EMPLOYEE",
+		 * "MANAGER"))
+		 * .withUser(users.username("Ranjith").password("lithi123").roles("EMPLOYEE",
+		 * "ADMIN"));
+		 */
+		auth.jdbcAuthentication().dataSource(secutiryDataSource);
 	}
 
 	@Override
